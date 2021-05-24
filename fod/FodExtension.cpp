3431282d93afa4b308887a70ce9317ff4aeb483b
Art_Chen <Chenxy0201@qq.com>
2021-05-24 10:42:47 +0200

    fod: Return touched z-order only
    OnePlus places FP layer upon the dimlayer, causing the former one
    highlighted. Only return the z-order with fingerprint pressed to
    target the layer we need.
    
    from https://github.com/LineageOS/android_device_oneplus_sm8150-common/commit/dd9a8be9726185bb8bee79db2f91a30ed8759f6d
    
    Change-Id: Iab08c6036f1d03e493a5532ab74b8ab1e9b61f2a

--- fod/FodExtension.cpp
+++ fod/FodExtension.cpp
@@ -16,8 +16,8 @@
 
 #include <compositionengine/FodExtension.h>
 
-uint32_t getFodZOrder(uint32_t, bool touched) {
-    return touched ? 0xfc8 : 0xbd6;
+uint32_t getFodZOrder(uint32_t z, bool touched) {
+    return touched ? 0xfc8 : z;
 }
 
 uint32_t getFodUsageBits(uint32_t usageBits, bool) {
